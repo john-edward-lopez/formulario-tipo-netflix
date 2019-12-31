bff5a50806b2d6477083c6133878bdd9edf8a48a
john lopez <johnguitarmusica@gmail.com>
2019-12-31 13:27:23 -0500

    commit inicial

---
+++ gulpfile.js
@@ -0,0 +1,39 @@
+const gulp = require('gulp');
+
+const sass = require('gulp-sass');
+
+const autoprefixer = require('gulp-autoprefixer');    
+
+
+function css(){
+
+   return gulp
+      .src('scss/app.scss')
+
+       .pipe( autoprefixer())
+
+       .pipe(sass({ outputStyle : 'expanded'})) // nested, compact , expanded , compressed
+
+       .pipe( gulp.dest('css'))
+
+
+
+
+}
+
+
+function watchArchivo()
+{
+   gulp.watch('scss/*.scss', css);
+   gulp.watch('*.html');
+   
+}
+
+
+//tareas
+
+gulp.task('css', css);
+gulp.task('watch', gulp.parallel(watchArchivo));
+
+
+
---
+++ js/scripts.js
@@ -0,0 +1,87 @@
+//validar formulario
+
+const inputs = document.querySelectorAll("form .campo input");
+
+console.log(inputs);
+
+// Agregar listener a los inputs o campos
+
+inputs.forEach(input => {
+  input.addEventListener("blur", validarInput);
+});
+
+inputs.forEach(input => {
+  input.addEventListener("input", validarInput);
+});
+
+function validarInput(e) {
+  const estado = ["valido", "no-valido"];
+
+  let clase;
+
+  if (e.target.value.length === 0) {
+    clase = estado[1];
+  } else {
+    clase = estado[0];
+  }
+  e.target.classList.remove(...estado);
+  e.target.nextElementSibling.classList.remove(...estado);
+  e.target.classList.add(clase);
+  e.target.nextElementSibling.classList.add(clase);
+
+  // inyectar dinamicamente el div con el error
+
+  if (clase === "no-valido") {
+    if (e.target.parentElement.nextElementSibling.classList[0] !== "alerta") {
+      //construir mensaje de error
+      const errorDiv = document.createElement("div");
+
+      errorDiv.appendChild(
+        document.createTextNode("Este campo es obligatorio")
+      );
+
+      errorDiv.classList.add("alerta");
+
+      //insertar el error
+
+      e.target.parentElement.parentElement.insertBefore(
+        errorDiv,
+        e.target.parentElement.nextElementSibling
+      );
+    }
+  } else {
+    //limpiar mensaje si existe
+    if (e.target.parentElement.nextElementSibling.classList[0] === "alerta") {
+      e.target.parentElement.nextElementSibling.remove();
+    }
+  }
+}
+
+//mostrar y quitar password
+
+const mostrarPasswordBtn = document.querySelector("form .campo span");
+
+mostrarPasswordBtn.addEventListener("click", e => {
+  const passwordInput = document.querySelector("#password");
+
+  if (e.target.classList.contains("mostrar")) {
+    //mostrar el texto
+
+    e.target.classList.remove("mostrar");
+
+    //cambiar el texto
+    e.target.textContent = "Ocultar";
+    //cambiamos a password
+
+    passwordInput.type = "text";
+  } else {
+    //mostrar el password
+    e.target.classList.add("mostrar");
+    //cambiar el texto
+    e.target.textContent = "Mostrar";
+
+    //cambiamos a password
+
+    passwordInput.type = "password";
+  }
+});
